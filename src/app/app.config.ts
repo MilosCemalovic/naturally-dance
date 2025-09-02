7129d1fabb7c4572485b223f99b12ca727922c5f
Milos Cemalovic <miloscemalovicnew@gmail.com>
2025-09-02 16:48:06 +0200

    PrimeNG app.config.ts setup

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,6 +5,9 @@ import { routes } from './app.routes'
 import { provideHttpClient } from '@angular/common/http'
 import { TranslocoHttpLoader } from './transloco-loader'
 import { provideTransloco } from '@jsverse/transloco'
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async'
+import { providePrimeNG } from 'primeng/config'
+import Aura from '@primeuix/themes/aura'
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -12,6 +15,19 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideHttpClient(),
+    provideAnimationsAsync(),
+    providePrimeNG({
+      inputVariant: 'filled',
+      ripple: true,
+      theme: {
+        preset: Aura,
+        options: {
+          prefix: 'p',
+          darkModeSelector: 'system',
+          cssLayer: false
+        }
+      }
+    }),
     provideTransloco({
       config: {
         availableLangs: ['en', 'rs'],
