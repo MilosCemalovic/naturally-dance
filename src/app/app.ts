ef3aaf834325f7cb3b9231b8846985a2ad14e342
Milos Cemalovic <miloscemalovicnew@gmail.com>
2025-09-02 16:49:53 +0200

    Navigation and Footer basic/test setup

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,13 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, signal } from '@angular/core'
+import { RouterOutlet } from '@angular/router'
+import { Navigation } from "./components/shared/navigation/navigation"
+import { Footer } from "./components/shared/footer/footer"
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Navigation, Footer],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('naturally-dance');
 }
