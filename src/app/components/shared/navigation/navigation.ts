e5d14f821576cfd39c8668bc4f2caefc51aeabdf
Milos Cemalovic <miloscemalovicnew@gmail.com>
2025-09-03 16:42:12 +0200

    Navigation component setup

--- src/app/components/shared/navigation/navigation.ts
+++ src/app/components/shared/navigation/navigation.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common'
+import { Component, } from '@angular/core'
+import { TranslocoModule, TranslocoService } from '@jsverse/transloco'
+import { MenubarModule } from 'primeng/menubar'
 
 @Component({
   selector: 'app-navigation',
-  imports: [],
+  standalone: true,
+  imports: [MenubarModule, CommonModule, TranslocoModule],
   templateUrl: './navigation.html',
   styleUrl: './navigation.scss'
 })
 export class Navigation {
+  items: any[] = []
+
+  constructor(private transloco: TranslocoService) {
+    // Translations are preloaded, so this should work fine
+    this.items = [
+      { label: this.transloco.translate('navigation.home'), icon: 'pi pi-home' },
+      { label: this.transloco.translate('navigation.about'), icon: 'pi pi-info-circle' },
+      { label: this.transloco.translate('navigation.projects'), icon: 'pi pi-trophy' }
+    ]
+  }
 
 }
