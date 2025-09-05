26d8a82d083ef0e58596e0c91cf7cdbc0dcc6230
Milos Cemalovic <miloscemalovicnew@gmail.com>
2025-09-05 15:50:49 +0200

    Language-switcher setup

---
+++ src/app/components/shared/language-switcher/language-switcher.ts
@@ -0,0 +1,39 @@
+import { CommonModule } from '@angular/common'
+import { Component } from '@angular/core'
+import { FormsModule } from '@angular/forms'
+import { TranslocoService } from '@jsverse/transloco'
+import { SelectModule } from 'primeng/select'
+import { LanguageService } from '../../../core/services/language.service'
+
+interface LanguageOption {
+  label: string
+  value: string
+  countryCode: string
+}
+
+@Component({
+  selector: 'app-language-switcher',
+  standalone: true,
+  imports: [SelectModule, FormsModule, CommonModule],
+  templateUrl: './language-switcher.html',
+  styleUrl: './language-switcher.scss'
+})
+export class LanguageSwitcher {
+  languages: LanguageOption[] = [
+    { label: 'English', value: 'en', countryCode: 'gb' },
+    { label: 'Srpski', value: 'rs', countryCode: 'rs' },
+  ]
+
+  selectedLanguage: LanguageOption
+
+  constructor(private transloco: TranslocoService, private languageService: LanguageService) {
+    const currentLanguage = this.transloco.getActiveLang()
+    this.selectedLanguage = this.languages.find((lang) => lang.value === currentLanguage) || this.languages[0]
+  }
+
+  swithchLanguage () {
+    if (this.selectedLanguage) {
+      this.languageService.switchLanguage(this.selectedLanguage.value)
+    }
+  }
+}
