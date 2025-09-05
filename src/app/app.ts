a5971bec77a800b9b9562078b9f97252b2e03ee7
Milos Cemalovic <miloscemalovicnew@gmail.com>
2025-09-05 15:49:43 +0200

    Language service setup inside app.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,9 @@
-import { Component, signal } from '@angular/core'
+import { Component, OnInit } from '@angular/core'
 import { RouterOutlet } from '@angular/router'
 import { Navigation } from "./components/shared/navigation/navigation"
 import { Footer } from "./components/shared/footer/footer"
+import { TranslocoService } from '@jsverse/transloco'
+import { LanguageService } from './core/services/language.service'
 
 @Component({
   selector: 'app-root',
@@ -9,5 +11,15 @@ import { Footer } from "./components/shared/footer/footer"
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {
+export class App implements OnInit {
+  constructor(private transloco: TranslocoService, private languageService: LanguageService) {}
+
+  ngOnInit (): void {
+    this.languageService.initializeLanguage()
+
+    // Debugging: Log language translation
+    this.transloco.selectTranslate('navigation.home').subscribe((translation) => {
+      console.log('Translation for "navigation.home":', translation)
+    })
+  }
 }
