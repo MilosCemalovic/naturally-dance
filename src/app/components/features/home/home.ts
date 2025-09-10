41913da159be0b41fe3efa194cdc394fad821d0f
Milos Cemalovic <miloscemalovicnew@gmail.com>
2025-09-10 18:05:36 +0200

    Home component with cards initial setup

--- src/app/components/features/home/home.ts
+++ src/app/components/features/home/home.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core'
+import { TranslocoModule } from '@jsverse/transloco'
+import { Component, OnInit } from '@angular/core'
+import { AsyncPipe, CommonModule } from '@angular/common'
+import { CardModule } from 'primeng/card'
+import { Observable } from 'rxjs'
+import { Header } from "../../shared/header/header"
+import { DanceDataService } from '../../../core/services/dance-data.service'
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [Header, CommonModule, TranslocoModule, CardModule, AsyncPipe],
   templateUrl: './home.html',
   styleUrl: './home.scss'
 })
-export class Home {
+export class Home implements OnInit {
+  dances$: Observable<any>
 
+  constructor(private danceDataService: DanceDataService) {
+    this.dances$ = this.danceDataService.dances$
+  }
+
+  ngOnInit (): void {
+    this.danceDataService.loadDances().subscribe()
+  }
 }
