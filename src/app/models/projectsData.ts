665c78b23539a6f0ade4e6b6c368a3bab3dea8cc
Milos Cemalovic <miloscemalovicnew@gmail.com>
2025-10-14 14:30:12 +0200

    Project image srcSet images added in model projectData

--- src/app/models/projectsData.ts
+++ src/app/models/projectsData.ts
@@ -3,6 +3,7 @@ export interface Project {
   title: string
   description: string
   imageUrl: string
+  imageSrcSet: string
   socialLinks: SocialLink[]
 }
 
@@ -19,6 +20,11 @@ export const PROJECTS: Project[] = [
     title: 'discoParty.title',
     description: 'discoParty.description',
     imageUrl: 'assets/images/disco-party.jpg',
+    imageSrcSet: `
+    assets/images/disco-party-400.jpg 400w,
+    assets/images/disco-party-800.jpg 800w,
+    assets/images/disco-party-1200.jpg 1200w
+  `,
     socialLinks: [
       {
         platform: 'facebook',
@@ -39,6 +45,11 @@ export const PROJECTS: Project[] = [
     title: 'kizomba.title',
     description: 'kizomba.description',
     imageUrl: 'assets/images/kizomba-flashmob.jpg',
+    imageSrcSet: `
+    assets/images/kizomba-flashmob.jpg 400w,
+    assets/images/kizomba-flashmob.jpg 800w,
+    assets/images/kizomba-flashmob.jpg 1200w
+  `,
     socialLinks: [
       {
         platform: 'youtube',
@@ -53,6 +64,11 @@ export const PROJECTS: Project[] = [
     title: 'kizombaBanjaLuka.title',
     description: 'kizombaBanjaLuka.description',
     imageUrl: 'assets/images/kizomba-banja-luka.jpg',
+    imageSrcSet: `
+    assets/images/kizomba-banja-luka.jpg 400w,
+    assets/images/kizomba-banja-luka.jpg 800w,
+    assets/images/kizomba-banja-luka.jpg 1200w
+  `,
     socialLinks: [
       {
         platform: 'youtube',
@@ -67,6 +83,75 @@ export const PROJECTS: Project[] = [
         label: 'YouTube'
       }
     ]
-  }
+  },
+  {
+    id: 'newYearParties',
+    title: 'newYearParties.title',
+    description: 'newYearParties.description',
+    imageUrl: 'assets/images/new-year-dance-parties.jpg',
+    imageSrcSet: `
+    assets/images/new-year-dance-parties-400.jpg 400w,
+    assets/images/new-year-dance-parties-800.jpg 800w,
+    assets/images/new-year-dance-parties-1200.jpg 1200w
+  `,
+    socialLinks: [
+      {
+        platform: 'facebook',
+        url: 'https://www.facebook.com/media/set/?set=a.309874499511979&type=3',
+        icon: 'pi pi-facebook',
+        label: 'Facebook'
+      },
+      {
+        platform: 'facebook',
+        url: 'https://www.facebook.com/media/set/?set=a.2243893099244507&type=3',
+        icon: 'pi pi-facebook',
+        label: 'Facebook'
+      },
+      {
+        platform: 'facebook',
+        url: 'https://www.facebook.com/media/set/?set=a.176343196198444&type=3',
+        icon: 'pi pi-facebook',
+        label: 'Facebook'
+      },
+    ]
+  },
+  {
+    id: 'danceWorkshops',
+    title: 'danceWorkshops.title',
+    description: 'danceWorkshops.description',
+    imageUrl: 'assets/images/dance-workshops.jpg',
+    imageSrcSet: `
+    assets/images/dance-workshops-400.jpg 400w,
+    assets/images/dance-workshops-800.jpg 800w,
+    assets/images/dance-workshops-1200.jpg 1200w
+  `,
+    socialLinks: [
+      {
+        platform: 'facebook',
+        url: 'https://www.facebook.com/naturallydance/photos_albums',
+        icon: 'pi pi-facebook',
+        label: 'Facebook'
+      },
+    ]
+  },
+  {
+    id: 'danceClasses',
+    title: 'danceClasses.title',
+    description: 'danceClasses.description',
+    imageUrl: 'assets/images/dance-classes.jpg',
+    imageSrcSet: `
+    assets/images/dance-classes-400.jpg 400w,
+    assets/images/dance-classes-800.jpg 800w,
+    assets/images/dance-classes-1200.jpg 1200w
+  `,
+    socialLinks: [
+      {
+        platform: 'facebook',
+        url: 'https://www.facebook.com/naturallydance/photos_albums',
+        icon: 'pi pi-facebook',
+        label: 'Facebook'
+      },
+    ]
+  },
   // Add other projects: workshops, parties, etc.
 ]
